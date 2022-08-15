ac889f1dc8972534e746d926e46594522cfaef28
Concept42 <105056049+Concept42@users.noreply.github.com>
2022-08-15 13:24:41 +0200

    listUsers from db

--- pages/users.js
+++ pages/users.js
@@ -22,21 +22,34 @@ function Users(props) {
   }, [])
 
   const users = contacts.map((user, index) => (
-    <li key={index}>
-      <span>Naziv tvrtke: {user.companyName}</span>
-      <span>Ime: {user.firstName}</span>
-      <span>Prezime: {user.lastName}</span>
-      <span>Oib: {user.oib}</span>
-      <span>Email: {user.email}</span>
-      <span>Adresa: {user.adress}</span>
-      <span>Grad: {user.city}</span>
-      <span>Broj Telefona: {user.phoneNumber}</span>
+    <li
+      className='flex justify-between mt-2 border-solid border-b-2 items-center h-8'
+      key={index}
+    >
+      <span className='flex w-[12.5%]'>{user.companyName}</span>
+      <span className='flex w-[12.5%]'>{user.firstName}</span>
+      <span className='flex w-[12.5%]'>{user.lastName}</span>
+      <span className='flex w-[12.5%]'>{user.oib}</span>
+      <span className='flex w-[12.5%]'>{user.email}</span>
+      <span className='flex w-[12.5%]'>{user.adress}</span>
+      <span className='flex w-[12.5%]'>{user.city}</span>
+      <span className='flex w-[12.5%]'>{user.phoneNumber}</span>
     </li>
   ))
 
   return (
-    <div>
-      <ul>{users}</ul>
+    <div className='flex flex-col'>
+      <div className='flex  justify-between mb-4 h-8 items-center'>
+        <h3 className='flex w-[12.5%] '>Naziv tvrtke</h3>
+        <h3 className='flex w-[12.5%] '>Ime</h3>
+        <h3 className='flex w-[12.5%] '>Prezime</h3>
+        <h3 className='flex w-[12.5%] '>Oib</h3>
+        <h3 className='flex w-[12.5%] '>Email</h3>
+        <h3 className='flex w-[12.5%] '>Adresa</h3>
+        <h3 className='flex w-[12.5%] '>Grad</h3>
+        <h3 className='flex w-[12.5%] '>Broj Telefona</h3>
+      </div>
+      <ul className='flex flex-col '>{users}</ul>
     </div>
   )
 }
