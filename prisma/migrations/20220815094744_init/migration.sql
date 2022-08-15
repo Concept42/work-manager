dce362b6cdd228c26a27332a18b4828fbfc9b756
Concept42 <105056049+Concept42@users.noreply.github.com>
2022-08-15 11:48:30 +0200

    init prisma Contact table

--- prisma/migrations/20220815094744_init/migration.sql
+++ prisma/migrations/20220815094744_init/migration.sql
@@ -4,8 +4,12 @@ CREATE TABLE "Customer" (
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
     "companyName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
     "adress" TEXT NOT NULL,
     "city" TEXT NOT NULL,
     "oib" INTEGER NOT NULL,
     "phoneNumber" INTEGER NOT NULL
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_email_key" ON "Customer"("email");
