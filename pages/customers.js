349718b6c2c6e4f7e713cbfe8250ab920ab8242c
Concept42 <105056049+Concept42@users.noreply.github.com>
2022-08-18 19:40:52 +0200

    Customer CRUD
    GetData, ShowData, DeleteData work

--- pages/customers.js
+++ pages/customers.js
@@ -1,14 +1,58 @@
+import { useState, useEffect } from 'react'
 import CustomerList from '../components/LIsts/CustomerList'
-import AddNewCustomer from '../components/Forms/AddNewCustomer'
 
 function Customers() {
+  const [workOrders, setWorkOrders] = useState([])
+  const [users, setUsers] = useState([])
+  const [customers, setCustomers] = useState([])
+  const [status, setStatus] = useState([])
+
+  const fetchWorkOrderData = async () => {
+    const response = await fetch(`/api/customer/getOrderData`)
+    const result = await response.json()
+    setWorkOrders(result)
+  }
+
+  const fetchUsersData = async () => {
+    const response = await fetch(`/api/customer/getUserData`)
+    const result = await response.json()
+    setUsers(result)
+  }
+  const fetchCustomerData = async () => {
+    const response = await fetch(`/api/customer/getCustomerData`)
+    const result = await response.json()
+    setCustomers(result)
+  }
+  const fetchStatusData = async () => {
+    const response = await fetch(`/api/customer/getStatusData`)
+    const result = await response.json()
+    setStatus(result)
+  }
+
+  useEffect(() => {
+    fetchWorkOrderData()
+    fetchUsersData()
+    fetchCustomerData()
+    fetchStatusData()
+    console.log('workORders: ', workOrders)
+    console.log('users: ', users)
+  }, [])
+
   return (
     <div>
-      <CustomerList />
-      {/* Add new contact form component */}
-      <div className='flex mt-32 justify-center'>
-        <AddNewCustomer />
-      </div>
+      {/* Work order List */}
+      <section>
+        {customers.map((oneCustomer, id) => {
+          return (
+            <CustomerList
+              key={id}
+              users={users}
+              singleCustomer={oneCustomer}
+              status={status}
+            />
+          )
+        })}
+      </section>
     </div>
   )
 }
