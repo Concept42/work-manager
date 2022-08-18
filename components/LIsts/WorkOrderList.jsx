fe93cda732f6ff5ca27d7df88c0896b9ada4f139
Concept42 <105056049+Concept42@users.noreply.github.com>
2022-08-18 11:57:58 +0200

    WorkOrderList implementing crud

--- components/LIsts/WorkOrderList.jsx
+++ components/LIsts/WorkOrderList.jsx
@@ -3,6 +3,7 @@ import moment from 'moment'
 
 function WorkOrderList(props) {
   const [singleWorkOrder, setSingleWorkOrder] = useState([])
+  const [editMode, setEditMode] = useState(false)
 
   useEffect(() => {
     setSingleWorkOrder({
@@ -19,24 +20,90 @@ function WorkOrderList(props) {
     })
   }, [])
 
+  const handleDeleteWorkOrder = async (id) => {
+    const response = await fetch(`/api/customer/deleteWorkOrder`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id }),
+    })
+  }
+  const handleEditWorkOrder = async () => {
+    setEditMode(true)
+    console.log(editMode)
+  }
+
   return (
-    <ul key={singleWorkOrder.id}>
-      <li>ID: {singleWorkOrder.id}</li>
-      <li>
-        createdAt:
-        {moment(singleWorkOrder.createdAt).format('DD-MMM-YYYY, h:mm:ss')}
-      </li>
-      <li>
-        updatedAt:{' '}
-        {moment(singleWorkOrder.updatedAt).format('DD-MMM-YYYY, h:mm:ss')}
-      </li>
-      <li>title: {singleWorkOrder.title}</li>
-      <li>statusFlag: {singleWorkOrder.statusFlag}</li>
-      <li>customer: {singleWorkOrder.customer}</li>
-      {/* <li>customerId: {singleWorkOrder.customerId}</li> */}
-      <li>user: {singleWorkOrder.userName}</li>
-      {/* <li>userId: {singleWorkOrder.userId}</li> */}
-    </ul>
+    <div>
+      {!editMode ? (
+        <ul key={singleWorkOrder.id}>
+          <li>ID: {singleWorkOrder.id}</li>
+          <li>
+            createdAt:
+            {moment(singleWorkOrder.createdAt).format('DD-MMM-YYYY, h:mm:ss')}
+          </li>
+          <li>
+            updatedAt:
+            {moment(singleWorkOrder.updatedAt).format('DD-MMM-YYYY, h:mm:ss')}
+          </li>
+          <li>title: {singleWorkOrder.title}</li>
+          <li>discription: {singleWorkOrder.discription}</li>
+          <li>statusFlag: {singleWorkOrder.statusFlag}</li>
+          <li>customer: {singleWorkOrder.customer}</li>
+          {/* <li>customerId: {singleWorkOrder.customerId}</li> */}
+          <li>user: {singleWorkOrder.userName}</li>
+          {/* <li>userId: {singleWorkOrder.userId}</li> */}
+          <div>
+            <button
+              onClick={() => handleDeleteWorkOrder(singleWorkOrder.id)}
+              className='border-solid border-2 border-red-500'
+            >
+              Delete
+            </button>
+            <button
+              onClick={() => handleEditWorkOrder()}
+              className='border-solid border-2 border-red-500'
+            >
+              Edit
+            </button>
+          </div>
+        </ul>
+      ) : (
+        <form>
+          <div>
+            <h3 className=''>ID: {singleWorkOrder.id} </h3>
+            <h3 className=''>
+              createdAt:
+              {moment(singleWorkOrder.createdAt).format(
+                'DD-MMM-YYYY, h:mm:ss'
+              )}{' '}
+            </h3>
+            <h3 className=''>
+              updatedAt:
+              {moment(singleWorkOrder.updatedAt).format(
+                'DD-MMM-YYYY, h:mm:ss'
+              )}{' '}
+            </h3>
+          </div>
+          <div className=''>
+            <h3>
+              title: <input value={singleWorkOrder.title} />
+            </h3>
+
+            <span>discription:</span>
+            <input value={singleWorkOrder.discription} />
+            <h3 className=''>statusFlag: {singleWorkOrder.statusFlag} </h3>
+            <h3>
+              customer: <input value={singleWorkOrder.customer} />
+            </h3>
+            <h3>
+              user: <input value={singleWorkOrder.userName} />
+            </h3>
+          </div>
+        </form>
+      )}
+    </div>
   )
 }
 
--- pages/workorders.js
+++ pages/workorders.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react'
 import AddNewWorkOrder from '../components/Forms/AddNewWorkOrder'
 import WorkOrderList from '../components/LIsts/WorkOrderList'
 
-
 function WorkOrders() {
   const [workOrders, setWorkOrders] = useState([])
 
@@ -23,7 +22,6 @@ function WorkOrders() {
         workOrders.map((oneWorkOrder, id) => {
           return <WorkOrderList key={id} singleWorkOrder={oneWorkOrder} />
         })}
-      <AddNewWorkOrder />
     </div>
   )
 }
