fba9f240c0927b20531c8fd33e564b52f0ab7036
Concept42 <105056049+Concept42@users.noreply.github.com>
2022-08-21 16:24:24 +0200

    Basic Design
    Users page popup

--- pages/users.js
+++ pages/users.js
@@ -2,9 +2,11 @@ import { useState, useEffect } from 'react'
 import UsersList from '../components/LIsts/UsersList'
 import { Fab } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
+import AddNewUser from '../components/Forms/AddNewUser'
 
 function Users() {
   const [users, setUsers] = useState([])
+  const [handleOpen, setHandleOpen] = useState(false)
 
   const fetchUsersData = async () => {
     const response = await fetch(`/api/customer/getUserData`)
@@ -16,14 +18,23 @@ function Users() {
     fetchUsersData()
   }, [users])
 
-  const openAddUser = () => {}
+  const handleOpenPopup = () => {
+    setHandleOpen(!handleOpen)
+  }
 
   return (
     <>
       <section>
+        {handleOpen ? (
+          <div className='flex w-screen border-solid border-2 h-screen justify-center items-center '>
+            <AddNewUser />
+          </div>
+        ) : (
+          ''
+        )}
         <div className='flex justify-between'>
           <h1 className='text-[24px] font-extrabold  text-font'>Zaposlenici</h1>
-          <Fab onClick={openAddUser} color='primary' aria-label='add'>
+          <Fab onClick={handleOpenPopup} color='primary' aria-label='add'>
             <AddIcon />
           </Fab>
         </div>
