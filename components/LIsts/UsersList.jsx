03c5ebdc34d09e6cb01060ed5e4e680a72b43db4
Concept42 <105056049+Concept42@users.noreply.github.com>
2022-08-28 10:32:28 +0200

    Customers CRUD
    Starting delete

--- components/LIsts/UsersList.jsx
+++ components/LIsts/UsersList.jsx
@@ -1,74 +1,26 @@
-import { useState, useEffect } from 'react'
 import Avatar from '@mui/material/Avatar'
 import DotMenu from '../Ui/DotMenu'
-import Popup from '../Utility/Popup'
 
 function UsersList(props) {
-  const singleUser = props.singleUser
-  const componentId = props.componentId
-
-  const [workOrders, setWorkOrders] = useState([])
-
-  useEffect(() => {
-    // let newWorkOrders = []
-    // if (workOrders.length === 0) {
-    //   singleUser.workOrders.map((workOrder) => {
-    //     let result = {
-    //       id: workOrder.id,
-    //       createdAt: workOrder.createdAt,
-    //       updatedAt: workOrder.updatedAt,
-    //       title: workOrder.title,
-    //       discription: workOrder.discription,
-    //       statusFlag: workOrder.statusFlag,
-    //       customerId: workOrder.customerId,
-    //       userId: workOrder.userId,
-    //     }
-    //     newWorkOrders.push(result)
-    //     setWorkOrders(newWorkOrders)
-    //   })
-    // }
-    // // if (singleUser.length === 0) {
-    // //   setSingleUser(props.singleUser)
-    // // }
-  })
+  const oneUser = props.singleUser
+  const componentId = props.index
 
   return (
-    <>
-      <ul
-        className='flex w-full h-24 justify-between items-center bg-secondary rounded-xl my-5 text-fontGray font-normal'
-        key={singleUser?.id}
-      >
-        <li className='flex flex-[5] ml-6 gap-4 '>
-          <Avatar className='z-0' src={singleUser?.image} alt='' />
-          <div>
-            <h3 className='text-font font-bold'>{singleUser?.name}</h3>
-            <h3 className='text-fontGray'>{singleUser?.email}</h3>
-          </div>
-        </li>
-        <li className='flex flex-[1]'>{singleUser?.role}</li>
-        <li className='flex flex-[1]'>
-          <div>
-            <DotMenu singleUser={singleUser} componentId={componentId} />
-          </div>
-        </li>
-      </ul>
-      {/* <div>
-        <h1 className='text-[50px]'>Lista poslova</h1>
-        {workOrders.map((workOrder) => {
-          return (
-            <li key={workOrder.id}>
-              <h3>ID posla: {workOrder.id}</h3>
-              <h3 className='text-[20px]'>Naslov posla: {workOrder.title}</h3>
-              <h3>Kreirano: {workOrder.createdAt}</h3>
-              <h3>Završeno: {workOrder.updatedAt}</h3>
-              <h3>Opis posla: {workOrder.discription}</h3>
-              <h3>Stanje posla:{workOrder.statusFlag}</h3>
-            </li>
-          )
-        })}
-      </div> */}
-    </>
+    <li className='flex w-full h-24 justify-between items-center bg-secondary rounded-xl my-5 text-fontGray font-normal'>
+      <li className='flex flex-[5] ml-6 gap-4 '>
+        <Avatar className='z-0' src={oneUser?.image} alt='' />
+        <div>
+          <h3 className='text-font font-bold'>{oneUser?.name}</h3>
+          <h3 className='text-fontGray'>{oneUser?.email}</h3>
+        </div>
+      </li>
+      <li className='flex flex-[1]'>{oneUser?.role}</li>
+      <li className='flex flex-[1]'>
+        <div>
+          <DotMenu singleUser={oneUser} index={componentId} />
+        </div>
+      </li>
+    </li>
   )
 }
-
 export default UsersList
--- pages/users.js
+++ pages/users.js
@@ -1,32 +1,15 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import UsersList from '../components/LIsts/UsersList'
 import { Fab } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
 import AddNewUser from '../components/Forms/AddNewUser'
-import useOutsideClick from '../utils/useOutsideClick'
 import Loading from '../components/Ui/Loading'
 import Popup from '../components/Utility/Popup'
 import { useSelector, useDispatch } from 'react-redux'
 import { handleUserPopup, cancelButton } from '../slices/themeSlice'
-import {
-  fetchUsers,
-  deleteId,
-  deleteUser,
-  deleteUserState,
-} from '../slices/userSlice'
+import { deleteUser, deleteUserState } from '../slices/userSlice'
 import { users, getUsersStatus } from '../slices/userSlice'
 import DeleteMessage from '../components/Ui/DeleteMessage'
-import Avatar from '@mui/material/Avatar'
-import DotMenu from '../components/Ui/DotMenu'
-
-// export async function getServerSideProps() {
-//   const res = await fetch(`http://localhost:3000/api/customer/getUserData`)
-//   const users = await res.json()
-
-//   return {
-//     props: { users },
-//   }
-// }
 
 function Users() {
   const themeContext = useSelector((state) => state.themeContext)
@@ -48,30 +31,6 @@ function Users() {
     dispatch(deleteUserState())
     dispatch(cancelButton())
     dispatch(deleteUser(deleteId))
-  }
-
-  let RenderUsers = () => {
-    const userList = contextUsers.map((oneUser, index) => (
-      <li
-        key={index}
-        className='flex w-full h-24 justify-between items-center bg-secondary rounded-xl my-5 text-fontGray font-normal'
-      >
-        <li className='flex flex-[5] ml-6 gap-4 '>
-          <Avatar className='z-0' src={oneUser?.image} alt='' />
-          <div>
-            <h3 className='text-font font-bold'>{oneUser?.name}</h3>
-            <h3 className='text-fontGray'>{oneUser?.email}</h3>
-          </div>
-        </li>
-        <li className='flex flex-[1]'>{oneUser?.role}</li>
-        <li className='flex flex-[1]'>
-          <div>
-            <DotMenu singleUser={oneUser} index={index} />
-          </div>
-        </li>
-      </li>
-    ))
-    return userList
   }
 
   return (
@@ -128,7 +87,15 @@ function Users() {
               <span className='flex flex-[1]'>Akcije</span>
             </div>
             <ul>
-              <RenderUsers />
+              {contextUsers.map((singleUser, index) => {
+                return (
+                  <UsersList
+                    key={index}
+                    singleUser={singleUser}
+                    index={index}
+                  />
+                )
+              })}
             </ul>
           </section>
         </div>
